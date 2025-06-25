f364e944eabc35a4ded33dccff97045b4e71ff0b
vxzyfx's github <vxzyfx@gmail.com>
2025-06-25 13:46:12 +0800

    Update edgy.lua

--- lua/vxvim/plugins/extras/ui/edgy.lua
+++ lua/vxvim/plugins/extras/ui/edgy.lua
@@ -73,30 +73,6 @@ return {
         },
       }
 
-      if VxVim.has("neo-tree.nvim") then
-        local pos = {
-          filesystem = "left",
-          buffers = "top",
-          git_status = "right",
-          document_symbols = "bottom",
-          diagnostics = "bottom",
-        }
-        local sources = VxVim.opts("neo-tree.nvim").sources or {}
-        for i, v in ipairs(sources) do
-          table.insert(opts.left, i, {
-            title = "Neo-Tree " .. v:gsub("_", " "):gsub("^%l", string.upper),
-            ft = "neo-tree",
-            filter = function(buf)
-              return vim.b[buf].neo_tree_source == v
-            end,
-            pinned = true,
-            open = function()
-              vim.cmd(("Neotree show position=%s %s dir=%s"):format(pos[v] or "bottom", v, VxVim.root()))
-            end,
-          })
-        end
-      end
-
       -- trouble
       for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
         opts[pos] = opts[pos] or {}
@@ -128,17 +104,6 @@ return {
         })
       end
       return opts
-    end,
-  },
-
-  -- prevent neo-tree from opening files in edgy windows
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
-          or { "terminal", "Trouble", "qf", "Outline", "trouble" }
-      table.insert(opts.open_files_do_not_replace_types, "edgy")
     end,
   },
 
