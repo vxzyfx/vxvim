bb96024f5a3455df76c232fb2f401ca70115c5df
shug <vxzyfx@gmail.com>
2025-07-14 17:22:36 +0800

    update: snacks image

--- lua/vxvim/plugins/ui.lua
+++ lua/vxvim/plugins/ui.lua
@@ -440,38 +440,18 @@ return {
   { "MunifTanjim/nui.nvim", lazy = true },
 
   {
-    "snacks.nvim",
+    "folke/snacks.nvim",
     opts = {
+      image = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
-      statuscolumn = { enabled = false }, -- we set this in options.lua
+      statuscolumn = { enabled = false },
       toggle = { map = VxUtil.safe_keymap_set },
       words = { enabled = true },
-    },
-    -- stylua: ignore
-    keys = {
-      {
-        "<leader>n",
-        function()
-          if Snacks.config.picker and Snacks.config.picker.enabled then
-            Snacks.picker.notifications()
-          else
-            Snacks.notifier.show_history()
-          end
-        end,
-        desc = "Notification History"
-      },
-      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    },
-  },
-
-  {
-    "folke/snacks.nvim",
-    opts = {
-      explorer = {},
+      explorer = { enabled = true },
       bigfile = { enabled = true },
       quickfile = { enabled = true },
       terminal = {
@@ -537,6 +517,18 @@ return {
       },
     },
     keys = {
+      {
+        "<leader>n",
+        function()
+          if Snacks.config.picker and Snacks.config.picker.enabled then
+            Snacks.picker.notifications()
+          else
+            Snacks.notifier.show_history()
+          end
+        end,
+        desc = "Notification History"
+      },
+      { "<leader>un",      function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
       {
         "<leader>fe",
         function()
