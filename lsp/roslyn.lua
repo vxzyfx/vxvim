19385e0aa6fc353da6c518cfc1d33458b4b69fc2
shug <vxzyfx@gmail.com>
2025-07-19 11:29:15 +0800

    update: stylua.toml

--- lsp/roslyn.lua
+++ lsp/roslyn.lua
@@ -3,15 +3,15 @@
 --- https://github.com/seblyng/roslyn.nvim
 ---
 return {
-  name = 'roslyn',
-  offset_encoding = 'utf-8',
+  name = "roslyn",
+  offset_encoding = "utf-8",
   cmd = {
-    'Microsoft.CodeAnalysis.LanguageServer',
-    '--logLevel=Information',
-    '--extensionLogDirectory=' .. vim.fs.joinpath(vim.uv.os_tmpdir(), 'roslyn_ls/logs'),
-    '--stdio',
+    "Microsoft.CodeAnalysis.LanguageServer",
+    "--logLevel=Information",
+    "--extensionLogDirectory=" .. vim.fs.joinpath(vim.fs.dirname(vim.lsp.get_log_path()), "roslyn"),
+    "--stdio",
   },
-  filetypes = { 'cs' },
+  filetypes = { "cs" },
   on_attach = function(client, buffer)
     VxUtil.lsp.on_attach(client, buffer)
   end,
@@ -24,11 +24,11 @@ return {
     },
   },
   settings = {
-    ['csharp|background_analysis'] = {
-      dotnet_analyzer_diagnostics_scope = 'fullSolution',
-      dotnet_compiler_diagnostics_scope = 'fullSolution',
+    ["csharp|background_analysis"] = {
+      dotnet_analyzer_diagnostics_scope = "fullSolution",
+      dotnet_compiler_diagnostics_scope = "fullSolution",
     },
-    ['csharp|inlay_hints'] = {
+    ["csharp|inlay_hints"] = {
       csharp_enable_inlay_hints_for_implicit_object_creation = true,
       csharp_enable_inlay_hints_for_implicit_variable_types = true,
       csharp_enable_inlay_hints_for_lambda_parameter_types = true,
@@ -42,15 +42,15 @@ return {
       dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
       dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
     },
-    ['csharp|symbol_search'] = {
+    ["csharp|symbol_search"] = {
       dotnet_search_reference_assemblies = true,
     },
-    ['csharp|completion'] = {
+    ["csharp|completion"] = {
       dotnet_show_name_completion_suggestions = true,
       dotnet_show_completion_items_from_unimported_namespaces = true,
       dotnet_provide_regex_completions = true,
     },
-    ['csharp|code_lens'] = {
+    ["csharp|code_lens"] = {
       dotnet_enable_references_code_lens = true,
     },
   },
--- lua/vxvim/plugins/formatting.lua
+++ lua/vxvim/plugins/formatting.lua
@@ -71,8 +71,8 @@ return {
       local opts = {
         default_format_opts = {
           timeout_ms = 3000,
-          async = false,           -- not recommended to change
-          quiet = false,           -- not recommended to change
+          async = false, -- not recommended to change
+          quiet = false, -- not recommended to change
           lsp_format = "fallback", -- not recommended to change
         },
         formatters_by_ft = {
