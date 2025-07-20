1c55ca7aba982d34949374eabfaee1cc59084747
shug <vxzyfx@gmail.com>
2025-07-20 11:18:54 +0800

    upadte: dap probe-rs

--- lua/vxvim/plugins/dap.lua
+++ lua/vxvim/plugins/dap.lua
@@ -62,6 +62,69 @@ return {
       require("overseer").enable_dap()
       vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
       local dap = require("dap")
+      dap.adapters["probe-rs-debug"] = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "probe-rs",
+          args = { "dap-server", "--port", "${port}" },
+        },
+      }
+      local probe_log_file = vim.fs.joinpath(vim.fs.dirname(vim.lsp.get_log_path()), "probe-rs.log")
+      dap.listeners.before["event_probe-rs-rtt-channel-config"]["plugins.nvim-dap-probe-rs"] = function(session, body)
+        local utils = require("dap.utils")
+        utils.notify(
+          string.format('probe-rs: Opening RTT channel %d with name "%s"!', body.channelNumber, body.channelName)
+        )
+        local file = io.open(probe_log_file, "a")
+        if file then
+          file:write(
+            string.format(
+              '%s: Opening RTT channel %d with name "%s"!\n',
+              os.date("%Y-%m-%d-T%H:%M:%S"),
+              body.channelNumber,
+              body.channelName
+            )
+          )
+        end
+        if file then
+          file:close()
+        end
+        session:request("rttWindowOpened", { body.channelNumber, true })
+      end
+      -- After confirming RTT window is open, we will get rtt-data-events.
+      -- I print them to the dap-repl, which is one way and not separated.
+      -- If you have better ideas, let me know.
+      dap.listeners.before["event_probe-rs-rtt-data"]["plugins.nvim-dap-probe-rs"] = function(_, body)
+        local message = string.format(
+          "%s: RTT-Channel %d - Message: %s",
+          os.date("%Y-%m-%d-T%H:%M:%S"),
+          body.channelNumber,
+          body.data
+        )
+        local repl = require("dap.repl")
+        repl.append(message)
+        local file = io.open(probe_log_file, "a")
+        if file then
+          file:write(message)
+        end
+        if file then
+          file:close()
+        end
+      end
+      -- Probe-rs can send messages, which are handled with this listener.
+      dap.listeners.before["event_probe-rs-show-message"]["plugins.nvim-dap-probe-rs"] = function(_, body)
+        local message = string.format("%s: probe-rs message: %s", os.date("%Y-%m-%d-T%H:%M:%S"), body.message)
+        local repl = require("dap.repl")
+        repl.append(message)
+        local file = io.open(probe_log_file, "a")
+        if file then
+          file:write(message)
+        end
+        if file then
+          file:close()
+        end
+      end
       dap.adapters.gdb = {
         type = "executable",
         command = "gdb",
